1d0fc4d5c39961092cf81726b539f01c56f74e20
jhongmez <gmezjhon@gmail.com>
2021-08-26 09:04:30 -0500

    Slick slider

--- src/app/components/home/home.module.ts
+++ src/app/components/home/home.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
+import { SlickCarouselModule } from 'ngx-slick-carousel';
 
 
 @NgModule({
@@ -11,7 +12,8 @@ import { HomeComponent } from './home.component';
   ],
   imports: [
     CommonModule,
-    HomeRoutingModule
+    HomeRoutingModule,
+    SlickCarouselModule
   ]
 })
 export class HomeModule { }
